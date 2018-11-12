b680a8ec0b1d86c0c404777fa0858e6a4aabcd75
Carlos Gonzalez <carlosge10@hotmail.com>
2018-11-11 23:25:05 -0600

    client funcitons working, carefull with server

--- cliente.py
+++ cliente.py
@@ -2,19 +2,74 @@ import socket
 import json
 HOST = '127.0.0.1'
 PORT = 6969
-def sendMsg(msg):
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((HOST, PORT))
-		msg = json.dumps(msg)
-		msg = bytes(msg, 'utf-8')
-		s.sendall(msg)
-		data = s.recv(1024)
-	print('Recieved', repr(data))
+
+def sendPublicMessage(s, nick):
+	message = input("Mensaje: ")
+	data = {"opt":1,"username":nick, "message":message}
+	response = sendMsg(data, s)
+	if response["status"] == 1 :
+		print("\n{},\n".format(response["description"]))
+def checkOnlineUsers(s, nick):
+	message = input("Mensaje: ")
+	data = {"opt":2,"username":nick, "message":message}
+	response = sendMsg(data, s)
+	if response["status"] == 1 :
+		print("\n{},\n".format(response["description"]))	
+def sendPrivateMessage(s, nick):
+	recipient = input("Destinatario: ")
+	message = input("Mensaje: ")
+	data = {"opt":3,"username":nick, "recipient":recipient, "message":message}
+	response = sendMsg(data, s)
+	if response["status"] == 1 :
+		print("\n{},\n".format(response["description"]))
+def exitChat(s, nick):
+	data = {"opt":4,"username":nick}
+	response = sendMsg(data, s)
+	if response["status"] == 1 :
+		print("\n{},\n".format(response["description"]))
+	elif response["status"] == 0 :	
+		print("You're offline.")
+def showmenu():
+	print("\n1) Enviar un mensaje público que será enviado a todos los usuarios.")
+	print("\n2) Consultar los usuarios conectados.")
+	print("\n3) Enviar un mensaje privado a un usuario.")
+	print("\n4) Salir del chat.")
+	option = input("\nQue quieres hacer? ")
+	return option
+
+def sendMsg(msg, s):
+	s.connect((HOST, PORT))
+	msg = json.dumps(msg)
+	msg = bytes(msg, 'utf-8')
+	s.sendall(msg)
+	data = s.recv(1024)
+	#data = repr(data)
+	response = json.loads(data)
+	return response
 	
 def main():
 	nick = input("Nick: ")
-	msg = {"username":nick}
-	sendMsg(msg)
+	msg = {"opt":0,"username":nick}
+
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		response = sendMsg(msg, s)
+		if response["status"] != 0 :
+			print("Oops")
+			return 
+		else :
+			print("welcome to the chat, {}".format(nick))
+			opt = showmenu()
+			if opt == "1" :
+				sendPublicMessage(s, nick)
+			elif opt=="2" :
+				checkOnlineUsers(s, nick)
+			elif opt=="3" :
+				sendPrivateMessage(s, nick)
+			elif opt=="4" :
+				exitChat(s, nick)
+			else :
+				print("nothing")
 
 if(__name__=="__main__"):
-    main()
+	main()
+
--- server.py
+++ server.py
@@ -15,15 +15,47 @@ def main():
 					break
 				print(data)
 				obj = json.loads(data)
-				if len(obj["username"]) == 0 :
-					print("no nick found")
-					data = {"status":1, "description":"no nick found"}
-					conn.sendall(data)
-				else :
-					print( "welcome, {}".format(obj["username"]))
+				if obj["opt"] == 0 :
+					if len(obj["username"]) == 0:
+						print("no nick found")
+						data = {"status":1, "description":"no nick found"}
+						conn.sendall(data)
+					else :
+						print( "welcome, {}".format(obj["username"]))
+						data = {"status":0}
+						data = json.dumps(data)
+						data = bytes(data, 'utf-8')
+						conn.sendall(data)
+
+				elif obj["opt"] == 1:
+					print(obj)
 					data = {"status":0}
 					data = json.dumps(data)
 					data = bytes(data, 'utf-8')
 					conn.sendall(data)
+				elif obj["opt"] == 2:
+					print(obj)
+					data = {"status":0}
+					data = json.dumps(data)
+					data = bytes(data, 'utf-8')
+					conn.sendall(data)
+				elif obj["opt"] == 3:
+					print(obj)
+					data = {"status":0}
+					data = json.dumps(data)
+					data = bytes(data, 'utf-8')
+					conn.sendall(data)
+				elif obj["opt"] == 4:
+					print(obj)
+					data = {"status":0}
+					data = json.dumps(data)
+					data = bytes(data, 'utf-8')
+					conn.sendall(data)
+				else :			
+					print(obj)
+					data = {"status":0}
+					data = json.dumps(data)
+					data = bytes(data, 'utf-8')
+					conn.sendall(data)	
 if(__name__=="__main__"):
 	main()
